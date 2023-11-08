a4cc5832eb1fe6f3d1896b9b4256001c7b049a71
Darigaazedge <105022024+Darigaazedge@users.noreply.github.com>
2023-11-07 16:10:35 -0800

    sorting based on last name then first name complete

--- src/components/contacts/contact-list.jsx
+++ src/components/contacts/contact-list.jsx
@@ -3,9 +3,32 @@ import Contact from "./contact";
 
 export default function Contacts() {
   const items = useAsyncValue();
+
+  const extractLastName = (fullName) => {
+    const cleanedName = fullName
+      .replace(/^(Mr|Mrs|Ms|Miss|Dr)\.?\s+/i, "")
+      .trim();
+    const words = cleanedName.split(" ");
+    return words[words.length - 1];
+  };
+
+  const sortedItems = items.slice().sort((a, b) => {
+    const lastNameA = extractLastName(a.name).toLowerCase();
+    const lastNameB = extractLastName(b.name).toLowerCase();
+
+    const firstNameA = a.name.split(" ")[0].toLowerCase();
+    const firstNameB = b.name.split(" ")[0].toLowerCase();
+
+    if (lastNameA !== lastNameB) {
+      return lastNameA.localeCompare(lastNameB);
+    } else {
+      return firstNameA.localeCompare(firstNameB);
+    }
+  });
+
   return (
     <ol className="list-decimal space-y-4">
-      {items.map((contact) => (
+      {sortedItems.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ol>
