ad061e762c85bc2c24faf34961cc1ac25af3204b
Manav Misra <manavm@visionwebsoft.com>
2023-11-30 18:17:50 -0600

    perf: :zap: move functions out of component
    Includes TODO comments 💡.

--- src/components/contacts/contact-list.jsx
+++ src/components/contacts/contact-list.jsx
@@ -1,24 +1,30 @@
 import { useAsyncValue } from "react-router-dom";
 import Contact from "./contact";
 
+// TODO{jeremy.eastman}: Consider what is already in '/src/utils.js' and if it can be used here.
+// Alternatively, consider moving this type of stuff into some 'utils' file instead of having it here.
+// Also, `extractFirstName` and `extractLastName` are very similar.
+// Consider refactoring them into one function, such as `spitName` or something.
+
+// Keep 'utility functions' out of the component file to avoid re-creating them on every render ⚡.
+const extractLastName = (fullName) => {
+  const cleanedName = fullName
+    .replace(/^(Mr|Mrs|Ms|Miss|Dr)\.?\s+/i, "")
+    .trim();
+  const words = cleanedName.split(" ");
+  return words[words.length - 1];
+};
+
+const extractFirstName = (fullName) => {
+  const cleanedName = fullName
+    .replace(/^(Mr|Mrs|Ms|Miss|Dr)\.?\s+/i, "")
+    .trim();
+  const words = cleanedName.split(" ");
+  return words.length > 1 ? words[0] : null;
+};
+
 export default function Contacts() {
   const items = useAsyncValue();
-
-  const extractLastName = (fullName) => {
-    const cleanedName = fullName
-      .replace(/^(Mr|Mrs|Ms|Miss|Dr)\.?\s+/i, "")
-      .trim();
-    const words = cleanedName.split(" ");
-    return words[words.length - 1];
-  };
-
-  const extractFirstName = (fullName) => {
-    const cleanedName = fullName
-      .replace(/^(Mr|Mrs|Ms|Miss|Dr)\.?\s+/i, "")
-      .trim();
-    const words = cleanedName.split(" ");
-    return words.length > 1 ? words[0] : null;
-  };
 
   const sortedItems = items.slice().sort((a, b) => {
     const lastNameA = extractLastName(a.name).toLowerCase();
